fa89a8f63efb192d1a7cba52f2b2127b48763bd0
Jackson Wang <Jackson_Wang@epam.com>
2024-04-16 18:12:11 +0800

    fix/menu-style+switch-participant-issue

--- packages/ui/src/components/ChatBox/OptionPopper.jsx
+++ packages/ui/src/components/ChatBox/OptionPopper.jsx
@@ -1,8 +1,8 @@
-import { Box, ClickAwayListener, MenuItem, Popper } from '@mui/material';
+import { Menu, MenuItem } from '@mui/material';
 import { useCallback } from 'react';
 
 
-export default function OptionPopper ({
+export default function OptionPopper({
   anchorEl,
   setAnchorEl,
   options,
@@ -18,26 +18,24 @@ export default function OptionPopper ({
   }, [handleClickAway, handleSelect])
 
   const open = Boolean(anchorEl);
-  const id = open ? 'simple-popper' : undefined;
 
   return (
-    <ClickAwayListener onClickAway={handleClickAway} >
-      <Popper
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        placement='top-start'
-      >
-        <Box sx={{ border: 1, p: 1, bgcolor: 'background.paper' }}>
-          {
-            (options || []).map((option, i) => {
-              return (
-                <MenuItem key={i} onClick={handleSelectClose(option)}>{option.name}</MenuItem>
-              )
-            })
-          }
-        </Box>
-      </Popper>
-    </ClickAwayListener>
+    <Menu
+      id="basic-menu"
+      anchorEl={anchorEl}
+      open={open}
+      onClose={handleClickAway}
+      MenuListProps={{
+        'aria-labelledby': 'basic-button',
+      }}
+    >
+      {
+        (options || []).map((option, i) => {
+          return (
+            <MenuItem key={i} onClick={handleSelectClose(option)}>{option.name}</MenuItem>
+          )
+        })
+      }
+    </Menu>
   );
 }
