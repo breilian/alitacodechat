0a9eb42442f0e93152f91fcae2754f8f814b10d1
Aliaksei_Breilian <aliaksei.breilian@bt.com>
2024-07-21 19:50:07 +0300

    feat: config and server for testing

---
+++ packages/ui/test/plugin/readme.md
@@ -0,0 +1,41 @@
+## Overview: JetBrains plugin and react chat UI.
+
+**_vscodeRef.current_** object is substituted by our new one **_with custom postMessage method_**.  
+Custom postMessage **_intercepts all messages_** from chat to VS Code plugin.  
+It does **_not call original window.postMessage_** to sent message to the plugin.  
+Instead of this **_it does http GET_** call to http://calltoide.com in order **_to get some ideResponse_**.  
+And now retrieved ideResponse is used as **_input for native window.postMessage_** to the chat - window.postMessage(ideResponse).  
+
+The aim of current plugin server is substitution of real JetBrains (or VS Code) plugin for testing or debugging purposes.
+This approach is suitable only for **webpack** managed version of chat.
+
+## Plugin Server
+
+Plugin server receives some requests from chat UI, calls to Alita and responses with some data.
+
+Base command to start server:
+npm start -- -sh https://eye.projectalita.ai -st TOKEN_VALUE -spid PROJECT_ID_VALUE -muid UID_VALUE
+
+Required parameters:  
+-sh: LLM Server host  
+-st: LLM Server token  
+-spid LLM Server project id  
+-muid Model integration UID  
+
+Optional parameters:  
+-p: Port to start plugin server. Default value is _3333_.  
+-ms: Path to json file with model settings. Default value is _model-settings.json_ which is in the root of project.  
+
+## React chat UI with webpack
+
+To start UI execute the command below from folder packages/ui:  
+npx webpack serve --mode development --env plugin=http://localhost:3333**
+
+Parameter --env serves to pass host of plugin server. Default value is http://calltoide.com
+
+By default, react chat UI is available to interact by http://localhost:8080/
+Use option --port to specify another port (_npx webpack serve --mode development --port 1234 ..._)
+
+Now you need to run javascript debug on webpack port (default is 8080) from IDE.
+VS Code: https://code.visualstudio.com/docs/nodejs/reactjs-tutorial#_debugging-react
+WebStorm: https://www.jetbrains.com/help/webstorm/react.html#react_debug_via_js_debug_rc
---
+++ packages/ui/test/plugin/server.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const cors = require('cors');
+const { program } = require('commander');
+const { readFileSync } = require("fs");
+
+program
+    .option('-p, --port <type>', 'Port to start local plugin stub server', 3333)
+
+    .requiredOption('-sh, --llm-server-host <type>', 'LLM Server host')
+    .requiredOption('-st, --llm-server-token <type>', 'LLM Server token')
+    .requiredOption('-spid, --llm-server-project-id <type>', 'LLM Server project id')
+
+    .option('-ms, --model-settings <type>', 'Path to model settings json', 'model-settings.json')
+    .requiredOption('-muid, --model-uid <type>', 'LLM Server integration uid');
+
+program.parse(process.argv);
+
+const host = program.opts().llmServerHost;
+const projectId = program.opts().llmServerProjectId;
+const token = program.opts().llmServerToken;
+
+async function getData(path) {
+    const baseUrl = new URL(`${host}/api/v1/`);
+    const fullUrl = new URL(path, baseUrl);
+
+    const response = await fetch(fullUrl, {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Alita-agent': 'IntelliJ IDEA',
+            'Content-Type': '*/*'
+        }
+    });
+    return await response.json();
+}
+
+const port = program.opts().port;
+const app = express();
+
+app.use(cors());
+app.listen(port, () => {
+    console.log(`Plugin stub server: http://localhost:${port}`);
+})
+app.get('/', async (req, res) => {
+    const type = req.query.type;
+    const chatData = req.query.data;
+
+    if (type) {
+        let data;
+
+        switch (type) {
+            case 'extension.getSelectedText':
+                data = 'Some stubbed selected text';
+                break;
+            case 'extension.getSocketConfig':
+                data = {
+                    projectId,
+                    host,
+                    token,
+                    path: '/socket.io'
+                }
+                break;
+            case 'extension.getModelSettings':
+                data = JSON.parse(
+                    readFileSync(program.opts().modelSettings, 'utf-8')
+                );
+                data.model.integration_uid = program.opts().modelUid;
+                break;
+            case 'extension.getPrompts':
+                data = (await getData(`prompt_lib/prompts/prompt_lib/${projectId}`)).rows;
+                break;
+            case 'extension.getDatasources':
+                data = (await getData(`datasources/datasources/prompt_lib/${projectId}`)).rows;
+                break;
+            case 'extension.getApplications':
+                data = (await getData(`applications/applications/prompt_lib/${projectId}`)).rows;
+                break;
+            case 'extension.getPromptDetail':
+                data = (await getData(`prompt_lib/prompt/prompt_lib/${projectId}/${chatData}`));
+                break;
+            case 'extension.getDatasourceDetail':
+                data = (await getData(`datasources/datasource/prompt_lib/${projectId}/${chatData}`));
+                break;
+            case 'extension.getApplicationDetail':
+                data = (await getData(`applications/application/prompt_lib/${projectId}/${chatData}`));
+                break;
+            case 'extension.getDeployments':
+                data = (await getData(`integrations/integrations/default/${projectId}`));
+                break;
+            case 'extension.copyCodeToEditor':
+                data = '';
+                break;
+            default:
+                data = `unexpected type of message: ${type}`;
+                break;
+        }
+
+        res.send(JSON.stringify({
+            data,
+            id: req.query.id,
+            type: type.replace('extension', 'ui'),
+        }));
+    } else {
+        res.send("THERE IS NO EXPECTED PARAMETER: TYPE");
+    }
+})
